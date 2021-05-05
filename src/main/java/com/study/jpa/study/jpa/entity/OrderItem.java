fdf9205a913034cda5368bde4a0730393eddd837
최홍희 <vvshinevv@gmail.com>
2021-05-05 20:59:16 +0900

    엔티티 만들어보기

---
+++ src/main/java/com/study/jpa/study/jpa/entity/OrderItem.java
@@ -0,0 +1,30 @@
+package com.study.jpa.study.jpa.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Getter @Setter
+@Entity
+@Table(name = "ORDER_ITEM")
+public class OrderItem {
+
+    @Id @GeneratedValue
+    @Column(name = "ORDER_ITEM_ID")
+    private Long id;
+
+    @Column(name = "ITEM_ID")
+    private Long itemId;
+
+    @Column(name = "ORDER_ID")
+    private Long orderId;
+
+    private int orderPrice;
+    private int count;
+
+}
